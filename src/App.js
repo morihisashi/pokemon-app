5597ee82411b7eaf5003d89b534415bc6a45fb06
morihisashi <hisa.1995.322@outlook.com>
2023-04-02 13:29:23 +0900

    navigation barの実装

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon, getPokemon } from './utils/pokemon.js';
 import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
 
 
 function App() {
@@ -35,6 +36,8 @@ function App() {
   console.log(pokemonData);
 
   return (
+    <>
+    <Navbar />
     <div className="App">
       {loading ? (
         <h1>ロード中・・・</h1>
@@ -46,6 +49,7 @@ function App() {
         </div>
       )}
     </div>
+    </>
   );
 }
 
